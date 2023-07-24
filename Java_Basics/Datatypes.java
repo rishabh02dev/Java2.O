e4ab57a42807394d559542f440532a68478bf2fd
kumkumshukla631@gmail.com <kumkumshukla631@gmail.com>
2023-07-25 01:07:29 +0530

    Input in Java

---
+++ Java_Basics/Datatypes.java
@@ -0,0 +1,13 @@
+public class Datatypes {
+    public static void main(String[] args) {
+        byte b = 8;
+        char a = 'a';
+        boolean var = true;
+        boolean vars = false;
+        float price = 10.5f;
+        //long
+        //double
+        
+
+    }
+}
