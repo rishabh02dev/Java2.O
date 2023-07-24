80043f154557d62e0d319ec482c40e729a8f7d76
kumkumshukla631@gmail.com <kumkumshukla631@gmail.com>
2023-07-24 23:14:47 +0530

    Day 1

---
+++ Java_Basics/Pattern.java
@@ -0,0 +1,8 @@
+public class Pattern {
+    public static void main(String[] args) {
+        System.out.print("****\n");
+         System.out.print("***\n");
+          System.out.print("**\n");
+           System.out.print("*\n");
+    }
+}
