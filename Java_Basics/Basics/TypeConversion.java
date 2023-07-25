41a54ad93e6283ebd09421970ac2817f29735cc7
kumkumshukla631@gmail.com <kumkumshukla631@gmail.com>
2023-07-25 11:59:02 +0530

    Operators in Java

---
+++ Java_Basics/Basics/TypeConversion.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class TypeConversion {
+    public static void main(String[] args) {
+        int a = 25;
+        long b = a;
+        System.out.println(b);
+        Scanner sc = new Scanner(System.in);
+        int number = sc.nextInt();
+        System.out.println(number);
+    }
+}
---
+++ Java_Basics/Basics/Typecasting.java
@@ -0,0 +1,7 @@
+public class Typecasting{
+    public static void main(String[] args) {
+        float a = 25.12f;
+        int b = (int)a;
+        System.out.println(b);
+    }
+}
