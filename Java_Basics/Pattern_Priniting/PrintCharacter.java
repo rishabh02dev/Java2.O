69b7e04a89b8c4799d683eb1ecc844a2d06c9d5a
kumkumshukla631@gmail.com <kumkumshukla631@gmail.com>
2023-07-27 13:05:24 +0530

    Character Pattern

--- Java_Basics/Pattern_Priniting/PrintCharacter.java
+++ Java_Basics/Pattern_Priniting/PrintCharacter.java
@@ -1,3 +1,17 @@
 public class PrintCharacter {
+    public static void main(String[] args) {
+        int n =4;
+    char ch = 'A';
+
+    //outer loop
+    for(int line =1; line<=n; line++){
+        //inner loop
+        for(int chars =1; chars<=line; chars++){
+            System.out.print(ch);
+            ch++;
+        }
+        System.out.println();
+    }
+    }
     
 }
