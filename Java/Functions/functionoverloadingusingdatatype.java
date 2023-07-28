91b234f65749e629600fc9031817974f3c03ae95
kumkumshukla631@gmail.com <kumkumshukla631@gmail.com>
2023-07-28 19:57:53 +0530

    Function overloading using arguements

--- Java/Functions/functionoverloadingusingdatatype.java
+++ Java/Functions/functionoverloadingusingdatatype.java
@@ -6,8 +6,13 @@ public class functionoverloadingusingdatatype {
     public static float sum(float a, float b){
         return a+b;
     }
+    public static double sum(double a, double b){
+        return a+b;
+    }
     public static void main(String[] args) {
         System.out.println(sum(5,6));
         System.out.println(sum(1.2f,0.8f));
+        System.out.println(sum(2.2,4.5));
     }
+
 }
