33ef783b1c718e2906c428a0ea3358accb435383
kumkumshukla631@gmail.com <kumkumshukla631@gmail.com>
2023-07-29 12:41:53 +0530

    Isprime optimised Approach

---
+++ Java/Functions/checkprime.java
@@ -0,0 +1,31 @@
+public class checkprime {
+    // public static boolean isPrime(int n) {
+    //     if(n==2){
+    //         return true;
+    //     }
+    //     for (int i = 2; i < n - 1; i++) {
+    //         if (n % i == 0) {
+    //             return false;
+    //         }
+    //     }
+    //     return true;
+    // }
+
+    //optimised approach
+
+    public static boolean isPrime(int n){
+        if(n==2){
+            return true;
+        }
+        for(int i =2; i<Math.sqrt(n); i++){
+            if(n%i == 0){
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(isPrime(7));
+    }
+}
