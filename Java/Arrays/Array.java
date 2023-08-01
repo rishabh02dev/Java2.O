9741487708d5dd84d58ea31abcfde030906c444a
kumkumshukla631@gmail.com <kumkumshukla631@gmail.com>
2023-08-01 21:30:02 +0530

    Adding comments

--- Java/Arrays/Array.java
+++ Java/Arrays/Array.java
@@ -1,29 +1,21 @@
 import java.util.Scanner;
 
-public class Array{
+public class Array {
     public static void main(String[] args) {
         int marks[] = new int[100];
+
         Scanner sc = new Scanner(System.in);
-
         System.out.println("length of the array is  " + marks.length);
-
 
         // marks[0] = sc.nextInt(); //phy
         // marks[1] = sc.nextInt(); //che
         // marks[2] = sc.nextInt(); //maths
 
         // System.out.println("physics: " + marks[0]);
-        //  System.out.println("chemistry: " + marks[1]);
-        //   System.out.println("maths: " + marks[2]);
+        // System.out.println("chemistry: " + marks[1]);
+        // System.out.println("maths: " + marks[2]);
         // int percentage = (marks[0] + marks[1] + marks[2])/3;
         // System.out.println("percentage = " + percentage + "% " );
 
-        //length of an Array
-
-
-
-
-           
-       
     }
 }
