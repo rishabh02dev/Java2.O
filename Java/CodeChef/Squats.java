c7f9c43a1244768c1a95e57394a30c82cc17b4e5
kumkumshukla631@gmail.com <kumkumshukla631@gmail.com>
2023-08-02 14:14:55 +0530

    Squats

---
+++ Java/CodeChef/Squats.java
@@ -0,0 +1,19 @@
+java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc = new Scanner(System.in);
+		int t = sc.nextInt();
+		for (int i = 0; i<t; i++){
+		    int x = sc.nextInt();
+		    int squats = (x*15);
+		    System.out.println(squats);
+		} 
+	}
+}
+
