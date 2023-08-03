376ef6f2bbb2c01251ca3888b200de032d0f0d11
kumkumshukla631@gmail.com <kumkumshukla631@gmail.com>
2023-08-04 02:41:03 +0530

    Learning sql problem

---
+++ Java/CodeChef/learning_sql.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class learning_sql{
+    	public static void main (String[] args) throws java.lang.Exception
+	{
+        Scanner sc = new Scanner(System.in);
+	        int r = sc.nextInt();
+	        int c = sc.nextInt();
+	        int e = sc.nextInt();
+	        System.out.println((r+c)*e);
+	    
+	}
+}
