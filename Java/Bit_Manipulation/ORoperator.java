8fca5df2cb15d45cc9015b0bf6d1403e1d73e88e
kumkumshukla631@gmail.com <kumkumshukla631@gmail.com>
2023-09-18 19:25:53 +0530

    One Complement

---
+++ Java/Bit_Manipulation/ORoperator.java
@@ -0,0 +1,7 @@
+
+
+public class ORoperator {
+    public static void main(String[] args) {
+        System.out.println(5 | 6);
+    }
+}
---
+++ Java/Bit_Manipulation/XORoperator.java
@@ -0,0 +1,5 @@
+public class XORoperator {
+    public static void main(String[] args) {
+        System.out.println(5 ^ 6);
+    }
+}
