3c77df62a1b10c4eca22fb5e94decfd267490fe8
kumkumshukla631@gmail.com <kumkumshukla631@gmail.com>
2023-10-25 18:39:28 +0530

    Queue

---
+++ Java/Collections_in_Java/Queue_Collection.java
@@ -0,0 +1,21 @@
+import java.util.LinkedList;
+import  java.util.Queue;
+
+public class Queue_Collection {
+    public static void main(String[] args) {
+    Queue<Integer> queue = new LinkedList<>();
+
+    queue.offer(12);
+    queue.offer(24);
+    queue.offer(36);
+    queue.offer(48);
+    queue.offer(60);
+    System.out.println(queue);
+
+      
+       
+
+       
+
+    }
+}
--- Java/Collections_in_Java/Stack_Collection.java
+++ Java/Collections_in_Java/Stack_Collection.java
@@ -1,12 +1,15 @@
+import java.util.Queue;
 import java.util.Stack;
 
 public class Stack_Collection{
     public static void main(String[] args) {
+    
         Stack<String> animals = new Stack<>();
         animals.push("Lion");
         animals.push("Horse");
         animals.push("Cat");
         animals.push("Dog");
+        
         // System.out.println("Animals are " + animals);
 
         System.out.println(animals.peek());
