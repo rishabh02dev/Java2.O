51701ae4dc12bade17b7f0dd308af3a850a07d14
kumkumshukla631@gmail.com <kumkumshukla631@gmail.com>
2023-10-25 18:14:21 +0530

    .

---
+++ Java/Collections_in_Java/Stack_Collection.java
@@ -0,0 +1,17 @@
+import java.util.Stack;
+
+public class Stack_Collection{
+    public static void main(String[] args) {
+        Stack<String> animals = new Stack<>();
+        animals.push("Lion");
+        animals.push("Horse");
+        animals.push("Cat");
+        animals.push("Dog");
+        // System.out.println("Animals are " + animals);
+
+        System.out.println(animals.peek());
+        animals.pop();
+        System.out.println(animals.peek());
+
+    }
+}    
