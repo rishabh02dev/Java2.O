a5d5005c0a5c79b245a2ca178b0c070a4b670094
kumkumshukla631@gmail.com <kumkumshukla631@gmail.com>
2023-10-28 09:12:24 +0530

    Single level Inheritance

--- Java/OOPS/inheritance.java
+++ Java/OOPS/inheritance.java
@@ -1,5 +1,34 @@
 public class inheritance {
     public static void main(String[] args) {
-        
+        Fish shark = new Fish();
+        shark.eat();
+        shark.breathe();
+        shark.color = "red";
+        shark.fish = 23;
+        System.out.println(shark.color);
+        System.out.println(shark.fish);
+
     }
 }
+
+
+//Base class
+class Animal{
+    String color;
+    void eat(){
+        System.out.println("eats");
+    }
+
+    void breathe(){
+        System.out.println("breathes");
+    }
+}
+
+class Fish extends Animal{
+    int fish;
+
+    void swim(){
+        System.out.println("Swims in water");
+    }
+}
+
