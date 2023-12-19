813418d7502a9091ce92dfe372c7cb5a36beae53
kumkumshukla631@gmail.com <kumkumshukla631@gmail.com>
2023-12-19 17:12:06 +0530

    Learning Recursion

---
+++ Java/Recursion_Kunal_Kushwah/fibonacci.java
@@ -0,0 +1,14 @@
+public class fibonacci{
+    public static void main(String[] args) {
+        System.out.println(fibo(6));
+    }
+
+    static int fibo(int n){
+        if(n<2){
+            return n;
+        }
+        else{
+            return  fibo(n-1)+ fibo(n-2);
+        }
+    }
+}
