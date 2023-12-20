99b24e6a71b0bb5d0c8594284c4ea316adb0b12d
kumkumshukla631@gmail.com <kumkumshukla631@gmail.com>
2023-12-20 13:46:11 +0530

    CountZeroes problem

---
+++ Java/Recursion_Kunal_Kushwah/countzeroes.java
@@ -0,0 +1,23 @@
+public class countzeroes {
+    public static void main(String[] args) {
+        System.out.println(count(00));
+    }
+
+    static int count(int n){
+        return helper(n, 0);
+    }
+
+    private static int helper(int n, int c){
+        if(n==0){
+            return c;
+        }
+
+        int rem = n%10;
+        if(rem==0){
+            return helper(n/10, c+1);
+        }
+        else{
+            return helper(n/10, c);
+        }
+    }
+}
