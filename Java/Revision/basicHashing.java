a31141a487ac35f9f7bf87eac367654fcc67793f
Rishabh Shukla <kumkumshukla631@gmail.com>
2024-06-01 09:26:05 +0000

    Revisiong basicHashing

---
+++ Java/Revision/basicHashing.java
@@ -0,0 +1,30 @@
+// Online Java Compiler
+// Use this editor to write, compile and run your Java code online
+import java.util.*;
+class HelloWorld {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n;
+        n = sc.nextInt();
+        int arr[] = new int[n];
+        for(int i=0; i<n; i++){
+            arr[i] = sc.nextInt();
+        }
+        
+        //precompute
+        int hash[] = new int[13];
+        for(int i=0; i<n;i++){
+            hash[arr[i]] +=1;
+        }
+        int q;
+        q = sc.nextInt();
+        while(q--!=0){
+            int number;
+            number = sc.nextInt();
+            //fetching
+            System.out.println(hash[number]);
+        }
+        
+        
+    }
+}
